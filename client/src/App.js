740c086afda8c0026f4bc430b89fc526ed555fa3
Allison Marie <aliederman@icloud.com>
2017-11-08 07:19:44 -0500

    stuff is working

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import Nav from "./components/Nav/Nav"
-import Jumbotron from ".components/Jumbotron";
-import Search from ".components/Search";
-import NYTResults from "./components/NYTResults";
+import Jumbotron from "./components/Jumbotron/Jumbotron";
+import Search from "./components/Search/Search";
+import NYTResults from "./components/NYTResults/NYTResults";
+import Input from "./components/Input/Input";
 import API from "./utils/API";
 import { Saved, SavedArticle } from "./components/Saved";
 import { Container, Row, Col } from "./components/Grid";
@@ -11,59 +12,55 @@ import { Container, Row, Col } from "./components/Grid";
 
 
 class App extends Component {
-	state = {
-		articles: [],
-		articleSearch: ""
-	};
+    state = {
+        articles: [],
+        articleSearch: ""
+    };
 
-	handleInputChange = event => {
-		const {name, value } = event.target;
-		this.setState({
-			[name]: value
-		});
-	};
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
 
-	handleFormSubmit = event => {
-		event.preventDefault();
-		API.getArticles(this.state.articleSearch)
-		.then(res => this.setState({ articles: res.data }))
-		.catch(err => console.log(err));
-	};
+    handleFormSubmit = event => {
+        event.preventDefault();
+        API.getArticles(this.state.articleSearch)
+            .then(res => this.setState({ articles: res.data }))
+            .catch(err => console.log(err));
+    };
 
-	render() {
-		return (
-			<div>
-			<Nav />
-			<Jumbotron />
-			<Container>
-			<Row>
-			<Col size="md-12">
-			<form>
-			<Container>
-			<Row>
-			<Input
-				name="articleSearch"
-				value={this.state.articleSearch}
-				onChange={this.handleInputChange}
-				placeholder="Search for an article topic..."
-			/>
-			<Col size="sx-3 sm-2">
-			<Button
-				onClick={this.handleFormSubmit}
-				type="success"
-				className="input-lg"
-			>
-			Search
-			</Button>
-			</Col>
-			</Row>
-			</Container>
-			</form>
-			</Col>
-			</Row>
-	
-			);
-	}
+    render() {
+     return (
+     	<div>
+         <h1>This is working </h1>
+         	<Nav />
+         		<Jumbotron />
+         		<form>
+         		<h3>Search Topic</h3>
+         				<Input
+         					name="articleSearch"
+         					value={this.state.articleSearch}
+         					onChange={this.handleInputChange}
+         					placeholder="Search for article topic..."/>
+         		<h3>Start Year</h3>
+         				<Input
+         					name="articleSearch"
+         					value={this.state.articleSearch}
+         					onChange={this.handleInputChange}
+         					placeholder="Start Year.."/>
+         		<h3>End Year</h3>
+         				<Input
+         					name="articleSearch"
+         					value={this.state.articleSearch}
+         					onChange={this.handleInputChange}
+         					placeholder="End Year..."/>
+         					</form>
+        </div>
+
+    );
+}
 }
 
 export default App;
